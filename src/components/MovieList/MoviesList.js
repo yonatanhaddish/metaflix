bb1de30cfa0e3628b3f6679f173693c0f91cdb10
Yonatan Haddish <yonihailshn@gmail.com>
2022-07-09 09:33:58 -0400

    MovieList component refactor

--- src/components/MovieList/MoviesList.js
+++ src/components/MovieList/MoviesList.js
@@ -1,27 +1,39 @@
 import React from "react";
 import "./movielist.css";
+import Heart from "react-heart";
 
 const MovieList = (props) => {
   return (
     <>
-      <div>
-        <div className="movies-list-container">
-          {props.movies.map((movie, index) => (
-            <div className="movie-single">
+      <div className="container">
+        {props.movies.map((movie, index) => (
+          <div className="cards">
+            <div className="card-img">
               <img
                 src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
                 alt="movie"
                 className="single-image"
               />
             </div>
-          ))}
-        </div>
-        <div>
-        {/* <p>{movie.title}</p> */}
-        </div>
+            <div className="card-body">
+                <h1>Movie Title</h1>
+                <p>Release Date</p>
+            </div>
+            <div className="card-fav-icon">
+                <Heart style={{width: "1.5rem"}}/>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
 };
 
 export default MovieList;
+{
+  /* <img
+                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                alt="movie"
+                className="single-image"
+              /> */
+}
