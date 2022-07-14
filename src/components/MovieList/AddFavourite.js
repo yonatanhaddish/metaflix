687f11f349f09b2811624be2f49cdd28d0ad74d9
Yonatan Haddish <yonihailshn@gmail.com>
2022-07-14 14:25:34 -0400

    some styling

--- src/components/MovieList/AddFavourite.js
+++ src/components/MovieList/AddFavourite.js
@@ -19,17 +19,36 @@ const AddFavourite = (props) => {
     // saveToLocalStorege(favMovies);
   }
 
-const styleHeart = {
+const styleHeart01 = {
+  width: "2.5rem",
+  // fill: "red",
+};
+
+const styleHeart02 = {
   width: "1.5rem",
   // fill: "red",
 };
 
+useEffect(() => {
+  console.log(`my-fav-list-heart : ${index}`, activeHeart)
+  window.localStorage.setItem(`my-fav-list-heart : ${index}`, JSON.stringify(activeHeart))
+}, [activeHeart]);
+
+useEffect(() => {
+  const data = window.localStorage.getItem(`my-fav-list-heart : ${index}`);
+  if (data !== null) setActiveHeart(JSON.parse(data))
+  console.log(data)
+}, []);
+
+const data01 = window.localStorage.getItem(`my-fav-list-heart : ${index}`);
+console.log(`my-fav-list-heart : ${index} ${data01}`)
+
   return(
     <>
     <Heart
-              className= 'heart'
+              // className= {}
               isActive={activeHeart}
-              style={styleHeart}
+              style={styleHeart01}
               // onClick={() => props.handleFavouritesClick(movie)}
               onClick= {() => handleClick({movie, index})}
             />
